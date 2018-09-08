1126bad1277abd14f93156fc1442b4a1cd354632
zach <zte.zachary@gmail.com>
2018-09-08 16:53:58 -0700

    Add landing page component.

---
+++ src/Components/Pages/LandingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Grid from 'react-materialize';
+
+class LandingPage extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default LandingPage;
