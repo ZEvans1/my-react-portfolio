998bfeafd0ca99cac287a14a4b8d0d464c0035e6
zach <zte.zachary@gmail.com>
2018-09-08 18:14:53 -0700

    Switch to material-ui instead of react materialize.

--- src/Components/Pages/LandingPage.js
+++ src/Components/Pages/LandingPage.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import Grid from 'react-materialize';
+
 
 class LandingPage extends Component {
     render(){
         return(
             <div className="full-screen">
-                <Grid>
-                    
-                </Grid>
+                
             </div>
         )
     }
