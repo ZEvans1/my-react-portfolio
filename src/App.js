58b22405580bb326542b3297ebe65e6479537e82
zach <zte.zachary@gmail.com>
2018-09-08 20:23:21 -0700

    Change app bar color using withStyles.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    background: 'green'
+  }
+}
 
 class App extends Component {
+  
   render() {
     return (
-      <div className="App">
-        
+      <div>
+        <AppBar className={this.props.classes.root}>
+          <Toolbar color='secondary'>
+            <Typography variant="title">
+              Title
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles) (App);
