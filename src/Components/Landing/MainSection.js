10d418ee5213b10f4d3e156a1471bbfb4fb2b637
ZEvans1 <zte.zachary@gmail.com>
2018-09-13 14:41:57 -0700

    Setup layout for landing page.

--- src/Components/Landing/MainSection.js
+++ src/Components/Landing/MainSection.js
@@ -6,22 +6,24 @@ class MainSection extends Component {
   render() {
     return (
       <div className="main-section">
-        <p className="nav-circle-left">Fullstack Projects</p>
-        <div className="my-info">
-          <h1>Zach Evans</h1>
-          <h2>Fullstack & Mobile Developer</h2>
-          <div className="social-links">
-            <a href="https://www.linkedin.com/in/zach-t-evans/" rel="noopener noreferrer" target="_blank">
-              <i className="fa fa-linkedin-square" aria-hidden="true"/>
-            </a>
-            <a href="https://github.com/ZEvans1" rel="noopener noreferrer" target="_blank">
-              <i className="fa fa-github-square" aria-hidden="true"/>
-            </a>
+        <div className="main-row">
+          <p className="nav-circle-left">Fullstack Projects</p>
+          <div className="my-info">
+            <h1>Zach Evans</h1>
+            <h2>Fullstack & Mobile Developer</h2>
+            <div className="social-links">
+              <a href="https://www.linkedin.com/in/zach-t-evans/" rel="noopener noreferrer" target="_blank">
+                <i className="fa fa-linkedin-square" aria-hidden="true"/>
+              </a>
+              <a href="https://github.com/ZEvans1" rel="noopener noreferrer" target="_blank">
+                <i className="fa fa-github-square" aria-hidden="true"/>
+              </a>
+            </div>
           </div>
+          <p className="nav-circle-right">Mobile Projects</p>
         </div>
-        <p className="nav-circle-right">Mobile Projects</p>
-        <div>
-          <p className="nav-circle-bottom">Contact</p>
+        <div className="nav-circle-bottom">
+          <p>Contact</p>
         </div>
       </div>
     );
